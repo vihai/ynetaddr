cd7ca5154da3a3c5f2b3136b4a3e998217c25d78
Daniele Orlandi <daniele@orlandi.com>
2010-12-28 06:04:33 +0100

    Added further tests and fixed bugs

--- lib/netaddr.rb
+++ lib/netaddr.rb
@@ -2,4 +2,4 @@ require File.expand_path('../netaddr/mac_addr', __FILE__)
 require File.expand_path('../netaddr/ip_addr', __FILE__)
 require File.expand_path('../netaddr/ipv4_addr', __FILE__)
 require File.expand_path('../netaddr/ipv4_net', __FILE__)
-
+require File.expand_path('../netaddr/ipv4_if_addr', __FILE__)
