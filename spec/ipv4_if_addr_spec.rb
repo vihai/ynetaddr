62794c2ea2ad8638714936bba3be9926371d7e23
Daniele Orlandi <daniele@orlandi.com>
2017-06-07 15:57:09 +0200

    Added copyright notices

--- spec/ipv4_if_addr_spec.rb
+++ spec/ipv4_if_addr_spec.rb
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2014-2017, Daniele Orlandi
+#
+# Author:: Daniele Orlandi <daniele@orlandi.com>
+#
+# License:: You can redistribute it and/or modify it under the terms of the LICENSE file.
+#
 
 require 'ynetaddr'
 
